53ab4eced4ea635ac2eabbe8555fd1e6adf83caa
August von Trapp and Sky Rousse <august@>
2016-07-06 13:19:54 -0700

    passing tests for getter methods, feed method and update for Pet class

--- lib/pet.rb
+++ lib/pet.rb
@@ -19,4 +19,29 @@ class Pet
     @activity
   end
 
+  define_method(:is_alive?) do
+    if @food[:level] > 0 && @rest[:level] > 0 && @activity[:level] > 0
+      return true
+    else
+      return false
+    end
+  end
+
+
+  define_method(:feed) do
+    alive = is_alive?()
+    if(alive)
+      @food[:level] += 1
+      return food()[:level]
+    else
+      'your pet is dead, long live your pet'
+    end
+  end
+
+  define_method(:update) do |time = nil|
+    new_time = time ? time : Time.now()
+    @food[:level] = (@food[:level] - (( new_time - @food[:last_time] ) / 60) ).to_i
+    @rest[:level] = (@rest[:level] - (( new_time - @rest[:last_time] ) / 60) ).to_i
+    @activity[:level] = (@activity[:level] - (( new_time - @activity[:last_time] ) / 60) ).to_i
+  end
 end
--- spec/pet_spec.rb
+++ spec/pet_spec.rb
@@ -11,4 +11,28 @@ describe "Pet" do
       expect(my_pet.activity()[:level]).to eq(10)
     end
   end
+  describe '#is_alive?' do
+    it('checks vitals for a value greater than 0') do
+      my_pet = Pet.new('Fluffy')
+      expect(my_pet.is_alive?()).to eq(true)
+    end
+  end
+  describe '#feed' do
+    it('check if alive and if so increase food level by 1') do
+        my_pet = Pet.new('Fluffy')
+        expect(my_pet.feed()).to eq(11)
+    end
+  end
+  describe '#update' do
+    it('compare time objects to detract form vitals') do
+        my_pet = Pet.new('Fluffy')
+        my_pet.update((Time.now() + 2*60))
+        expect(my_pet.food()[:level]).to eq(7)
+        expect(my_pet.rest()[:level]).to eq(7)
+        expect(my_pet.activity()[:level]).to eq(7)
+    end
+  end
 end
+
+
+# @food[:level] += 1 - (@food[:last_time] - Time.now() / 60)
